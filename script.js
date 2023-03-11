5c03f21fa0e1a42a12a85dc986a38c353bac9eda
Aleksandar Miler <aleksandar.miler.85@gmail.com>
2023-03-11 10:07:17 +0100

    html- id's & js- showQuestion function added

--- script.js
+++ script.js
@@ -62,3 +62,21 @@ let questions = [
         "rightAnswer" : 2
     }
 ];
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('allQuestions').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question['question'];
+
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+
+}
