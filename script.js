a7412640fe932648f5be2b09ba493dc8d38b6ce7
Aleksandar Miler <aleksandar.miler.85@gmail.com>
2023-03-12 16:05:28 +0100

    functions calculatePrecentage() & resetGame() added

--- script.js
+++ script.js
@@ -68,41 +68,59 @@ let rightAnswer = 0;
 function init() {
     document.getElementById('allQuestions').innerHTML = questions.length;
     document.getElementById('rightAnswerNumber').innerHTML = rightAnswer;
-    
+
     showQuestion();
 }
 
 function showQuestion() {
     if (currentQuestion >= questions.length) {
-     // to do show endscreen 
-     document.getElementById('endScreen').style = "display: unset " ; // zeigt EndScreen DialogBox
-     document.getElementById('questionBody').style = "display: none" ; // versteckt Questions DialogBox
-     document.getElementById('picture').src = "img/pexels-anna-shvets-6250946.jpg"; //zeigt neues Bild wenn das Spiel zu Ende ist
-     document.getElementById('picture').classList.add('picture'); // das Bild bekommt css Einstellungen
+        //  show Endscreen 
+        document.getElementById('endScreen').style = "display: unset "; // zeigt EndScreen DialogBox
+        document.getElementById('questionBody').style = "display: none"; // versteckt Questions DialogBox
+        document.getElementById('picture').src = "img/pexels-anna-shvets-6250946.jpg"; //zeigt neues Bild wenn das Spiel zu Ende ist
+        document.getElementById('picture').classList.add('picture'); // das Bild bekommt css Einstellungen
 
-     document.getElementById('rightAnswerNumber').innerHTML = rightAnswer;  // Zahl der richtigbeantworteten FRagen wird angezeigt
+        document.getElementById('rightAnswerNumber').innerHTML = rightAnswer;  // Zahl der richtigbeantworteten FRagen wird angezeigt
 
     } else {
-    let question = questions[currentQuestion];
-    document.getElementById('numberOfQuestion').innerHTML = currentQuestion + 1;  // Zahl an welcher Frage wir uns mommentan befinden wird angezeigt
+        let question = questions[currentQuestion];
+        document.getElementById('numberOfQuestion').innerHTML = currentQuestion + 1;  // Zahl an welcher Frage wir uns mommentan befinden wird angezeigt
+        // if (currentQuestion > ) {
+            
+        // }
+        // calculatePrecentage();
 
-    document.getElementById('questionText').innerHTML = question['question'];  // Frage aus JSON wird angezeigt
-    
+        document.getElementById('questionText').innerHTML = question['question'];  // Frage aus JSON wird angezeigt
 
-    document.getElementById('answer_1').innerHTML = question['answer_1'];  // die nächste Fragen werden angezeigt
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
+
+        document.getElementById('answer_1').innerHTML = question['answer_1'];  // die nächste Fragen werden angezeigt
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
     }
+}
+
+function calculatePrecentage() {
+    
+    let percent = (currentQuestion + 1) / questions.length;  // Variable die das prozentuelle Verhältnis von insgesamte Fragen und Frage an der wir gerade sind
+    percent = Math.round(percent * 100);
+
+    document.getElementById('progress-bar').innerHTML = `${percent}%`;  // das prozentuelle Verhältni wird gezeigt
+
+    document.getElementById('progress-bar').style = `width:${percent}%`;  // style des HTML elementes wird direkt angesprochen und geändert
 }
 
 function answer(selection) {
     let question = questions[currentQuestion];
-    // document.getElementById(selection).innerHTML = question['answer_1'];
-    console.log('current Question is', selection); 
+
+    console.log('current Question is', selection);
     console.log('current Question is', question['rightAnswer']);
+
     let selectedQuestionNumber = selection.slice(-1);        // wir entnehmen dass letzte Symbol aus angeklikter Antwort
+
     console.log('selectedQuestionNumber is', selectedQuestionNumber);
+    calculatePrecentage();
+
     let idOfRightAnswer = `answer_${question['rightAnswer']}`;   //  das letzte Symbol aus Id wird zum Variable
     if (question['rightAnswer'] == selectedQuestionNumber) {
         //to do
@@ -119,16 +137,16 @@ function answer(selection) {
     document.getElementById('next-Question').disabled = false;     // Knopf um nächste Gruppe der Fragen anzuzeigen wird wieder aktiviviert
 }
 
-function nextQuestion() {    
-   currentQuestion++;                  // die nächste Gruppe der Fragen, bzw. nächstes Object aus JSON wird angezeigt
-   document.getElementById('next-Question').disabled = true;  // Knopf um nächste Gruppe der Fragen anzuzeigen wird wieder deaktiviviert
-   showQuestion(); 
-   removeClassButtons();
+function nextQuestion() {
+    currentQuestion++;                  // die nächste Gruppe der Fragen, bzw. nächstes Object aus JSON wird angezeigt
+    document.getElementById('next-Question').disabled = true;  // Knopf um nächste Gruppe der Fragen anzuzeigen wird wieder deaktiviviert
+    showQuestion();
+    removeClassButtons();
 }
 
 
 
-function removeClassButtons(){                      //alle Eigenschafften der Fragen werden entfernt
+function removeClassButtons() {                      //alle Eigenschafften der Fragen werden entfernt
     document.getElementById('answer_1').parentNode.classList.remove('bg-info');
     document.getElementById('answer_1').parentNode.classList.remove('bg-secondary');
 
@@ -140,4 +158,17 @@ function removeClassButtons(){                      //alle Eigenschafften der Fr
 
     document.getElementById('answer_4').parentNode.classList.remove('bg-info');
     document.getElementById('answer_4').parentNode.classList.remove('bg-secondary');
+}
+
+function resetGame() {
+    currentQuestion = 0;
+    rightAnswer = 0; 
+    document.getElementById('endScreen').style = "display:none ";
+    document.getElementById('questionBody').style = "";
+    document.getElementById('picture').src = "img/pexels-leeloo-thefirst-5428836.jpg"; 
+    document.getElementById('picture').classList.remove('picture');
+    document.getElementById('progress-bar').style = "width: 0%";
+    showQuestion(); 
+    document.getElementById('next-Question').disabled = true;
+
 }
